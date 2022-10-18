95c44957f6ff665ebae8236859f794961fc472ef
Monica <ramirez.92.monica@gmail.com>
2022-10-17 19:14:44 -0500

    changed some of the names around to render images more efficiently and to follow lab reqs.

--- src/Main.js
+++ src/Main.js
@@ -6,14 +6,14 @@ class Main extends React.Component {
     return (
       <main>
        <HornedBeast 
-       name= "Alpine Ibex" 
-       image =""
-       info ="This is a mysterious, wild goat that lives in the European alps. These creatures are adapted for thriving in open grasslands most often. Though Alpine ibex are obviously an exception – frequenting the monstrous and sublime heights of the alps." 
+       title= "Alpine Ibex" 
+       imageUrl=" https://factanimal.com/wp-content/uploads/2022/07/alpine-ibex-facts.jpg"
+       description ="This is a mysterious, wild goat that lives in the European alps. These creatures are adapted for thriving in open grasslands most often. Though Alpine ibex are obviously an exception – frequenting the monstrous and sublime heights of the alps." 
        />
        <HornedBeast 
-       name= "Dik-dik"
-       image =""
-       info ="Dik-dik are a kind of miniature antelope, with hooves and (on the males anyway) horns. Dik-diks grow to only a foot or so high at the shoulder, which means they can hide among the grasses, but they prefer places where they can see a fair distance. They’re herbivores that eat leaves, fruit, berries and plant shoots, which provide both sustenance and water."
+       title= "Dik-dik"
+       imageUrl ="https://cdn.britannica.com/35/151535-050-CB0E3646/Kirk-dik-dik.jpg"
+       description ="Dik-dik are a kind of miniature antelope, with hooves and (on the males anyway) horns. Dik-diks grow to only a foot or so high at the shoulder, which means they can hide among the grasses, but they prefer places where they can see a fair distance. They’re herbivores that eat leaves, fruit, berries and plant shoots, which provide both sustenance and water."
        />
       </main>
     )
