cc367e17b663176bc5c9c06ff607668fc63e3712
Monica <ramirez.92.monica@gmail.com>
2022-10-20 19:22:22 -0500

    edited to add handleSelect for form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // * import 
 
 import React from 'react';
-import Header from './Header.js';
+import Header from './Header/Header.js';
+import FormDropdown from './FormDropdown/FormDropdown.js';
+import Form from 'react-bootstrap/Form';
 import Footer from './Footer.js';
-import Main from './Main.js';
-import SelectedBeasts from './SelectedBeasts.js';
+import Main from './Main/Main.js';
+import SelectedBeasts from './SelectedBeasts/SelectedBeasts.js';
 import data from './data.json';
 
 //  *class components
@@ -14,6 +16,7 @@ class App extends React.Component{
     this.state ={
       showModal: false,
       selectedBeast:{},
+      sortedData: data,
 
     }
   }
@@ -32,18 +35,50 @@ handleOpenModal =(beast) => {
   })
 }
 
+handleSelect =(event) => {
+  let selected=event.target.value;
+  if (selected === '1'){
+    let newData =data.filter(animal => animal.horns === 1)
+    this.setState({
+      sortedData: newData
+    });
+}else if (selected === '2'){
+  let newData =data.filter(animal => animal.horns === 2)
+    this.setState({
+      sortedData: newData
+    });
+
+}else if (selected === '3'){
+  let newData =data.filter(animal => animal.horns === 3)
+    this.setState({
+      sortedData: newData
+    });
+
+}else if (selected === '100'){
+  let newData =data.filter(animal => animal.horns === 100)
+    this.setState({
+      sortedData: newData
+    });
+
+}else if(selected === '--'){
+
+}
+};
 
   render(){
     return(
       <>
       <Header/>
+      <FormDropdown
+      onChange ={this.handleSelect}/>   
       <SelectedBeasts
       show = {this.state.showModal}
       onHide = {this.handleCloseModal}
       selectedBeast ={this.state.selectedBeast}/>
       <Main 
       handleOpenModal ={this.handleOpenModal}
-      data={data}
+      sortedData={this.state.sortedData}
+
       />
       <Footer />
       </>
