97b113449837cedab07a5ab856b4d59c80c2ac5f
Monica <ramirez.92.monica@gmail.com>
2022-10-20 19:24:34 -0500

    created folders and migrated files accordingly

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,21 +1,19 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import React from "react";
-import HornedBeast from "./HornedBeast";
+import HornedBeast from "../HornedBeast/HornedBeast";
 import './Main.css'
 
 class Main extends React.Component {
   render(){
     // *render fires as soon as page gets mounted
-    let beasts = this.props.data.map((animal,index) => {
+    let beasts = this.props.sortedData.map((animal,index) => {
         return <HornedBeast
         title={animal.title}
         imageUrl={animal.image_url}
         description= {animal.description}
         // key is a unique identifier for each child, helps react keep track of elements in a list. used for whatever your mapping, using foreach
         key={index}
-        // because add hearts props can acccs prop beause it lives in app
-        // addHearts = {this.props.addHearts}
         handleOpenModal = {()=>this.props.handleOpenModal(animal)}
         />
       
